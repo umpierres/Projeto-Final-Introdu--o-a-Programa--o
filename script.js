07d5ecc7ba42670668142a2ff876683d32777d18
umpierres <alexandraumpierres@outlook.com>
2023-01-09 18:47:51 -0300

    adicionando localstorage

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+const form = document.querySelector('#form_criar_conta');
+function entrarConta() {
+    console.log('funciona');
+}
+
+function criarConta() {
+    localStorage.setItem('usuario', form.usuario.value);
+    localStorage.setItem('email', form.email.value);
+    localStorage.setItem('senha', form.senha.value);
+    localStorage.setItem('repitaSenha', form.repitaSenha.value);
+}
+
 let titulo_recado = document.querySelector('#titulo-recado');
 let mensagem_recado = document.querySelector('#mensagem-recado');
 let mensagens = [];
@@ -11,4 +23,5 @@ function salvarRecado() {
     };
     mensagens.push(recados);
     console.log(mensagens);
+    localStorage.setItem(mensagens);
 }
